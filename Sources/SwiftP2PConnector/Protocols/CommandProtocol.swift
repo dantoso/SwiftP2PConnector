4a40646d660d664063a3417def4df8c1b6d5eff4
dantoso <xeddantasvfd@gmail.com>
2022-07-06 16:10:31 -0300

    refactor: removed the command sender protocol

--- Sources/SwiftP2PConnector/Protocols/CommandProtocol.swift
+++ Sources/SwiftP2PConnector/Protocols/CommandProtocol.swift
@@ -4,15 +4,3 @@ import MultipeerConnectivity
 public protocol Command {
 	func action()
 }
-
-@available(iOS 11.0, *)
-public protocol CommandSender {
-	var commandKey: String {get set}
-}
-
-@available(iOS 11.0, *)
-public extension CommandSender {
-	func sendCommand(to peers: [MCPeerID]) {
-		P2PConnector.sendKey(commandKey, to: peers)
-	}
-}
