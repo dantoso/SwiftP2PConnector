3ef36ed3383d15753d2ead5091e40cfb72c68f43
dantoso <xeddantasvfd@gmail.com>
2023-02-13 20:34:53 -0300

    fix: removing useless code

--- Package.swift
+++ Package.swift
@@ -10,14 +10,10 @@ let package = Package(
         .library(
             name: "SwiftP2PConnector",
             targets: ["SwiftP2PConnector"]),
-		.library(
-			name: "MultipeerConnectivity",
-			targets: ["SwiftP2PConnector"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-		.package(path: "MultipeerConnectivity")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
